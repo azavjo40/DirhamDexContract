ca8f034f437269c0eb7a2d51c49996359e13f258
Azam Sufiev <asufiev@rightandabove.com>
2024-01-14 21:45:57 +0100

    Added prettier code

--- test/ExchangeV2.test.ts
+++ test/ExchangeV2.test.ts
@@ -1,11 +1,17 @@
-import { expect } from "chai";
-import { ethers, upgrades } from "hardhat";
-import { Signer } from "ethers";
-import { DIRHAM, Exchange, ExchangeV2, IERC20, User } from "../typechain-types";
-require("dotenv").config();
-const { DIRHAM_ADSRESS, COLD_WALLET, USDT_ADDRESS, USER_ADSRESS, EXCHANGE_ADDRESS } = process.env;
+import { expect } from 'chai';
+import { ethers, upgrades } from 'hardhat';
+import { Signer } from 'ethers';
+import { DIRHAM, Exchange, ExchangeV2, IERC20, User } from '../typechain-types';
+require('dotenv').config();
+const {
+  DIRHAM_ADSRESS,
+  COLD_WALLET,
+  USDT_ADDRESS,
+  USER_ADSRESS,
+  EXCHANGE_ADDRESS,
+} = process.env;
 
-describe("ExchangeV2 Contract", function () {
+describe('ExchangeV2 Contract', function () {
   let exchange: ExchangeV2;
   let dirham: DIRHAM;
   let user: User;
@@ -13,14 +19,20 @@ describe("ExchangeV2 Contract", function () {
   let owner: Signer;
   let addr1: Signer;
   let addr2: Signer;
-  const MINTER_ROLE = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("MINTER_ROLE"));
-  const MARKETING_ROLE = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("MARKETING_ROLE"));
-  const USER_ROLE = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("USER_ROLE"));
+  const MINTER_ROLE = ethers.utils.keccak256(
+    ethers.utils.toUtf8Bytes('MINTER_ROLE')
+  );
+  const MARKETING_ROLE = ethers.utils.keccak256(
+    ethers.utils.toUtf8Bytes('MARKETING_ROLE')
+  );
+  const USER_ROLE = ethers.utils.keccak256(
+    ethers.utils.toUtf8Bytes('USER_ROLE')
+  );
 
   beforeEach(async function () {
     [owner, addr1, addr2] = await ethers.getSigners();
 
-    const DIRHAM = await ethers.getContractFactory("DIRHAM");
+    const DIRHAM = await ethers.getContractFactory('DIRHAM');
 
     if (DIRHAM_ADSRESS) {
       dirham = DIRHAM.attach(DIRHAM_ADSRESS!);
@@ -28,7 +40,7 @@ describe("ExchangeV2 Contract", function () {
       dirham = await DIRHAM.deploy();
     }
 
-    const UserFactory = await ethers.getContractFactory("User");
+    const UserFactory = await ethers.getContractFactory('User');
 
     if (USER_ADSRESS) {
       user = UserFactory.attach(USER_ADSRESS!);
@@ -36,11 +48,15 @@ describe("ExchangeV2 Contract", function () {
       user = await UserFactory.deploy();
     }
 
-    const ExchangeFactory = await ethers.getContractFactory("ExchangeV2");
+    const ExchangeFactory = await ethers.getContractFactory('ExchangeV2');
     if (EXCHANGE_ADDRESS) {
       exchange = ExchangeFactory.attach(EXCHANGE_ADDRESS);
     } else {
-      exchange = (await upgrades.deployProxy(ExchangeFactory, [dirham.address, USER_ADSRESS, COLD_WALLET])) as Exchange;
+      exchange = (await upgrades.deployProxy(ExchangeFactory, [
+        dirham.address,
+        USER_ADSRESS,
+        COLD_WALLET,
+      ])) as Exchange;
       await exchange.deployed();
       // exchange = await ExchangeFactory.deploy();
     }
@@ -48,39 +64,47 @@ describe("ExchangeV2 Contract", function () {
     // await dirham.grantRole(MINTER_ROLE, exchange.address);
     // await exchange.grantRole(MARKETING_ROLE, owner.getAddress());
 
-    usdtToken = await ethers.getContractAt("IERC20", USDT_ADDRESS!);
+    usdtToken = await ethers.getContractAt('IERC20', USDT_ADDRESS!);
   });
 
-  describe("Deployment", function () {
-    it("Should set the right owner", async function () {
-      expect(await exchange.hasRole(await dirham.DEFAULT_ADMIN_ROLE(), owner.getAddress())).to.be.true;
+  describe('Deployment', function () {
+    it('Should set the right owner', async function () {
+      expect(
+        await exchange.hasRole(
+          await dirham.DEFAULT_ADMIN_ROLE(),
+          owner.getAddress()
+        )
+      ).to.be.true;
     });
 
-    it("Should set the right minter role", async function () {
+    it('Should set the right minter role', async function () {
       expect(await dirham.hasRole(MINTER_ROLE, exchange.address)).to.be.true;
     });
 
-    it("Should set the right marketing role", async function () {
-      expect(await exchange.hasRole(MARKETING_ROLE, owner.getAddress())).to.be.true;
+    it('Should set the right marketing role', async function () {
+      expect(await exchange.hasRole(MARKETING_ROLE, owner.getAddress())).to.be
+        .true;
     });
   });
 
-  describe("Buy token", function () {
-    it("Should allow buying tokens", async function () {
+  describe('Buy token', function () {
+    it('Should allow buying tokens', async function () {
       const ownerAddress = await owner.getAddress();
 
       if (!(await user.hasRole(USER_ROLE, ownerAddress))) {
-        await user.connect(owner).register("Azam", "Sufiev", 31);
+        await user.connect(owner).register('Azam', 'Sufiev', 31);
       }
 
       const initialBalance = await dirham.balanceOf(ownerAddress);
 
-      const amountToBuy = ethers.utils.parseUnits("1", 18);
+      const amountToBuy = ethers.utils.parseUnits('1', 18);
       await usdtToken.connect(owner).approve(exchange.address, amountToBuy);
 
-      const tx = await exchange.connect(owner).buyTokens(USDT_ADDRESS!, amountToBuy);
+      const tx = await exchange
+        .connect(owner)
+        .buyTokens(USDT_ADDRESS!, amountToBuy);
       const receipt = await tx.wait();
-      const event = receipt.events?.find((e) => e.event === "TokensPurchased");
+      const event = receipt.events?.find((e) => e.event === 'TokensPurchased');
 
       expect(event?.args?.buyer).to.equal(ownerAddress);
       expect(event?.args?.amount).to.equal(amountToBuy);
@@ -88,6 +112,32 @@ describe("ExchangeV2 Contract", function () {
       const newBalance = await dirham.balanceOf(ownerAddress);
       expect(newBalance.sub(initialBalance)).to.equal(amountToBuy);
     });
+
+    // it('Should allow withdraw tokens', async function () {
+    //   const ownerAddress = await owner.getAddress();
+    //   const userAddress = '0x298A98B7041DDf400b10183d3028287Ed5cF43D0';
+    //
+    //   const amountToWithdraw = ethers.utils.parseUnits('1', 18);
+    //   await usdtToken
+    //     .connect(owner)
+    //     .approve(exchange.address, amountToWithdraw);
+    //
+    //   const tx = await exchange
+    //     .connect(owner)
+    //     .withdraw(USDT_ADDRESS!, ownerAddress, amountToWithdraw, {
+    //       gasLimit: 2000000,
+    //     });
+    //
+    //   const receipt = await tx.wait();
+    //   const event = receipt.events?.find((e) => e.event === 'TokensWithdrawn');
+    //   console.log(event);
+    //
+    //   expect(event).to.exist;
+    //   expect(event?.args?.recipient).to.equal(userAddress);
+    //   expect(event?.args?.amount.toString()).to.equal(
+    //     amountToWithdraw.toString()
+    //   );
+    // });
   });
 });
 
